9497a13d269939a25de514076d144775d9400af7
untemps <6Mv2^2S6$2>
2021-01-30 21:26:33 +0100

    chore: Fix build config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-	plugins: ['@babel/plugin-proposal-class-properties'],
+    "presets": [
+        "@babel/preset-env"
+    ],
+    plugins: ['@babel/plugin-proposal-class-properties'],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -6,32 +6,39 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
+import {terser} from 'rollup-plugin-terser'
+
+const production = !!process.env.PRODUCTION
 
 export default {
-	input: 'src/index.js',
-	output: {
-		format: 'es',
-		file: 'dist/index.js',
-	},
-	plugins: [
-		svelte(),
-		css({ output: 'index.css' }),
-		replace({
-			'process.env.NODE_ENV': JSON.stringify('production'),
-		}),
-		babel({
-			exclude: 'node_modules/**',
-			babelHelpers: 'bundled',
-		}),
-		resolve({
-			dedupe: ['svelte'],
-		}),
-		commonjs(),
-		serve({
-			open: true,
-			contentBase: ['dist', 'public'],
-			port: 10002,
-		}),
-		livereload(),
-	],
+    input: 'src/index.js',
+    output: {
+        name: 'svelte-readotron',
+        file: 'index.js',
+        format: 'umd',
+    },
+    plugins: [
+        svelte(),
+        css({output: 'index.css'}),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled',
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
+        }),
+        resolve({
+            dedupe: ['svelte'],
+        }),
+        commonjs(),
+        production && terser(),
+        !production &&
+        serve({
+            open: true,
+            contentBase: ['public'],
+            port: 10003,
+        }),
+        !production &&
+        livereload()
+    ],
 }
