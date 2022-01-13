2864734b5a937a089ed3b2c8b1a61d2a35a422a3
untemps <v.lebadezet@untemps.net>
2022-01-13 19:32:11 +0100

    fix: Remove duplicate file and fix wrong import

--- src/components/Readotron.svelte
+++ src/components/Readotron.svelte
@@ -5,7 +5,7 @@
 
 	import interpolate from '../utils/interpolate'
 
-    import ScrollProgress from '../scroll/scrollprogress'
+    import ScrollProgress from '../scroll/Scrollprogress'
 
     export let selector
     export let lang = 'en'
