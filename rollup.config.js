bb7ed6f9880f3eac1580a8e92ac119c874d37f78
Vincent Le Badezet <v.lebadezet@untemps.net>
2022-07-19 20:27:34 +0200

    chore(force): Add rollup sizes plugin (#48)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import filesize from 'rollup-plugin-sizes'
 import { terser } from 'rollup-plugin-terser'
 import visualizer from 'rollup-plugin-visualizer'
 
@@ -31,6 +32,7 @@ export default {
 		resolve(),
 		commonjs(),
 		production && terser(),
+		filesize(),
 		visualizer({
 			sourcemap: true,
 		}),
